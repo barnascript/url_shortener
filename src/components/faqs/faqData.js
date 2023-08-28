b1686d88fbb29076d15daddd7f275279812518cc
barnabas <barnascript@gmil.com>
2023-08-28 11:06:49 +0100

    added footer

--- src/components/faqs/faqData.js
+++ src/components/faqs/faqData.js
@@ -3,25 +3,25 @@ const faqData = [
     id: 1,
     question: "What is a URL shortener?",
     answer: `A URL shortener, or a link shortener, simplifies long and complicated URLs into brief, comprehensible links. The application of a URL shortener can notably elevate your digital marketing effortss. When you choose to shorten URL, you're making a complex web address more user-friendly and accessible.
-For instance, a URL such as rb.gy/BakingMasterclass immediately communicates the content of the link, making it more inviting for users to click.
+For instance, a URL such as cx.co/masterclasss immediately communicates the content of the link, making it more inviting for users to click.
 For anyone producing or circulating content online, understanding how to shorten a URL is essential.
 Enhance your online presence with our effortless and free URL shortening tool presented above.`,
   },
   {
     id: 2,
     question: "What is a custom URL shortener?",
-    answer: `A custom URL shortener, like Rebrandly, facilitates the transformation of long URLs into succinct, brand-enhancing links. Rather than a random set of letters and numbers, a custom URL shortener allows you to include identifiable words or phrases. This tool enables you to convert a standard URL into a brand-relevant, shortened URL, thereby enhancing brand visibility and recall and building trust with your audience.`,
+    answer: `A custom URL shortener, like CX Connections, facilitates the transformation of long URLs into succinct, brand-enhancing links. Rather than a random set of letters and numbers, a custom URL shortener allows you to include identifiable words or phrases. This tool enables you to convert a standard URL into a brand-relevant, shortened URL, thereby enhancing brand visibility and recall and building trust with your audience.`,
   },
   {
     id: 3,
     question: "How do I shorten a URL for free?",
-    answer: `You can shorten a URL for free using online services like RB.GY. The process couldn't be simpler: access RB.GY's URL shortener tool, insert your long URL into the specified field, then click to generate a shortened URL.`,
+    answer: `You can shorten a URL for free using online services like cx.co. The process couldn't be simpler: access cx.co's URL shortener tool, insert your long URL into the specified field, then click to generate a shortened URL.`,
   },
   {
     id: 4,
     question: "What is a URL shortener?",
     answer: `A URL shortener, or a link shortener, simplifies long and complicated URLs into brief, comprehensible links. The application of a URL shortener can notably elevate your digital marketing effortss. When you choose to shorten URL, you're making a complex web address more user-friendly and accessible.
-For instance, a URL such as rb.gy/BakingMasterclass immediately communicates the content of the link, making it more inviting for users to click.
+For instance, a URL such as cx.co/BakingMasterclass immediately communicates the content of the link, making it more inviting for users to click.
 For anyone producing or circulating content online, understanding how to shorten a URL is essential.
 Enhance your online presence with our effortless and free URL shortening tool presented above.`,
   },
@@ -33,22 +33,22 @@ Enhance your online presence with our effortless and free URL shortening tool pr
   {
     id: 6,
     question: "Can I use a domain I already own?",
-    answer: `No. Rebrandly is a web-based service requiring no software to install or code to write. After subscribing, you will be ready to start branding and shortening links right away. No tech skills are required!`,
+    answer: `No. Cx Connections is a web-based service requiring no software to install or code to write. After subscribing, you will be ready to start branding and shortening links right away. No tech skills are required!`,
   },
   {
     id: 7,
     question: "How do I know your service is reliable and scalable?",
-    answer: `Rebrandly is the industry-leading solution for millions of users who want a powerful way to brand and shorten links. The Rebrandly platform is hosted on Amazon Web Services in multiple locations with guaranteed 99.9% uptime and public uptime reports available 24/7.`,
+    answer: `Cx Connections is the industry-leading solution for millions of users who want a powerful way to brand and shorten links. The Cx Connections platform is hosted on Amazon Web Services in multiple locations with guaranteed 99.9% uptime and public uptime reports available 24/7.`,
   },
   {
     id: 8,
     question: "How do I change a long URL to a short URL?",
-    answer: `To shorten a URL, you'll need a service like RB.GY. First, copy the long URL you want to shorten. Then, access the URL shortener tool. Paste your long URL into the appropriate field, then click "Shorten URL." RB.GY will instantly generate a shortened version of your original URL, which leads to the same page when clicked.`,
+    answer: `To shorten a URL, you'll need a service like cx.co. First, copy the long URL you want to shorten. Then, access the URL shortener tool. Paste your long URL into the appropriate field, then click "Shorten URL." cx.co will instantly generate a shortened version of your original URL, which leads to the same page when clicked.`,
   },
   {
     id: 9,
     question: "Which link shortener is best?",
-    answer: `Rebrandly stands out as a top-rated URL shortener due to its dependability, security, and array of link management features. Beyond merely providing a way to shorten URL, Rebrandly also delivers performance tracking through click-through rates and the option to brand URLs. Explore Rebrandly's premium features to fully optimize your link management.`,
+    answer: `Cx Connections stands out as a top-rated URL shortener due to its dependability, security, and array of link management features. Beyond merely providing a way to shorten URL, Rebrandly also delivers performance tracking through click-through rates and the option to brand URLs. Explore Rebrandly's premium features to fully optimize your link management.`,
   },
   {
     id: 10,
@@ -63,16 +63,17 @@ Enhance your online presence with our effortless and free URL shortening tool pr
   {
     id: 12,
     question: "Can I create a custom plan with other features/options?",
-    answer: `Rebrandly has many out-of-the-box branded link shortener options, but if you have specific needs that differ from the plans you see here, we'd love to chat about how we can help. Get in touch today!`,
+    answer: `Cx Connections has many out-of-the-box branded link shortener options, but if you have specific needs that differ from the plans you see here, we'd love to chat about how we can help. Get in touch today!`,
   },
   {
     id: 13,
     question: "Do you have a special plan for Enterprise customers?",
-    answer: `At Rebrandly, we understand that Enterprise businesses often have more complex workflows and needs. We work with many global customers and have a dedicated team to help you identify the right plan and get up and running. If you need to discuss a solution custom-designed for your Enterprise, please get in touch.`,
+    answer: `At Cx Connections, we understand that Enterprise businesses often have more complex workflows and needs. We work with many global customers and have a dedicated team to help you identify the right plan and get up and running. If you need to discuss a solution custom-designed for your Enterprise, please get in touch.`,
   },
   {
     id: 14,
-    question: "I have more questions about Rebrandly, how can I contact you?",
+    question:
+      "I have more questions about Cx Connections, how can I contact you?",
     answer: `We'd love to connect! Let’s discuss how Rebrandly can help brand and shorten your links so they perform as needed. Please get in touch with us here! get in touch with us here.`,
   },
 ];
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,10 +2,24 @@ import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={`sw ${styles.wrapper}`}>
-      <small className={styles.email}>barnascript@gmail.com</small>{" "}
-      <small className={styles.copyright}>©Copyright 2023</small>
-      <small className={styles.rights}>All rights reserved</small>
+    <div className={`fw ${styles.wrapper}`}>
+      <div className={`fw ${styles.wrapper1}`}>
+        <span className={`sw ${styles.powered_by}`}>
+          <i>Powered by</i> <b>CX Connections</b>
+        </span>
+      </div>
+      <div className={`fw ${styles.wrapper_2}`}>
+        <div className={`sw ${styles.container}`}>
+          <ul className={styles.links}>
+            <li>Terms and Conditions</li> <li>Privacy Policy</li>{" "}
+            <li>Cookie Policy</li> <li>Anti Spam Policy</li>{" "}
+            <li>Report abuse</li>
+          </ul>
+          <small className={styles.rights}>
+            CX Connections ©Copyright 2023
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
